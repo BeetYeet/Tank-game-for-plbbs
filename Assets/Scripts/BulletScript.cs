fd69553896f2b0fe21f0553de9a9f85d6228c25b
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-11-27 10:59:43 +0100

    Made a shooting script and a bullet script

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+	public Rigidbody rb;
+	float damage;
+
+	public void Initialize(float damage)
+	{
+		this.damage = damage;
+	}
+
+	private void Update()
+	{
+		transform.LookAt(transform.position + rb.velocity, Vector3.up);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		Destroy(gameObject);
+	}
+}
