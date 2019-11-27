2d395faa59c01972a1ac04774c2af59f41ec8fb0
Sven Viktor Krantz <viktor.krantz@elev.ga.lbs.se>
2019-11-27 09:44:14 +0100

    I fixed a soundmanager and made it easier to add more sounds. Made it so that you can die ingame and has health.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Health : MonoBehaviour
+{
+    public int objectHealth;
+    public SoundManager sSource;
+    public AudioClip hitSound1;
+    public AudioClip hitSound2;
+
+    private void Start()
+    {
+        //SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+    }
+
+    void Update()
+    {
+        if (objectHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            objectHealth -= 1;
+            SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+        }
+    }
+}
