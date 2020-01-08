4b89ac55e49f228714c97f05bd2aadef635d4b6f
Sven Viktor Krantz <viktor.krantz@elev.ga.lbs.se>
2020-01-08 09:12:23 +0100

    fixade grejer

---
+++ Assets/StartRestartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartRestartMenu : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+    public Canvas restartCanvas;
+
+    void Update()
+    {
+        if(player1 == null || player2 == null)
+        {
+        }
+
+
+    }
+}
