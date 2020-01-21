3eff4414b078fc384365a7bab0bd38831c275771
Victor Tuong <victor.tuong@elev.ga.lbs.se>
2020-01-21 22:00:51 +0100

    Popup text done

--- Assets/Scripts/FloatingText.cs
+++ Assets/Scripts/FloatingText.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FloatingText : MonoBehaviour
 {
     public float destroyTime;
-    public Vector3 offset = new Vector3(5f, 1000, 0);
+    public Vector3 offset = new Vector3(5f, 0, 0);
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,29 +5,109 @@ using UnityEngine.Audio;
 
 public class Health : MonoBehaviour
 {
-	public int objectHealth;
-	public AudioClip hitSound1;
-	public AudioClip hitSound2;
-	private void Start()
-	{
-		//SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-	}
+    public int objectHealth;
+    public AudioClip hitSound1;
+    public AudioClip hitSound2;
 
-	void Update()
-	{
-		if (objectHealth <= 0)
-		{
-			Destroy(gameObject);
-		}
-		if (Input.GetKeyDown(KeyCode.H))
-		{
-			SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-		}
-	}
+    public int speedUp;
+    public int damageUp;
+    public int healthUp;
 
-	public void DoDamage(int damage)
-	{
-		objectHealth -= damage;
-		SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-	}
+    public int pickUpNumber;
+    public GameObject floatingTextPrefab;
+
+    //Refrencing the scripts
+    public MovementTest move;
+    public PlayerShooting hurt;
+
+
+    private void Start()
+    {
+        //SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+        move = GameObject.FindObjectOfType<MovementTest>();
+        hurt = GameObject.FindObjectOfType<PlayerShooting>();
+    }
+
+    void Update()
+    {
+        if (objectHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+        }
+    }
+
+    public void DoDamage(int damage)
+    {
+        objectHealth -= damage;
+        SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Pickup")
+        {
+            pickUpNumber = Random.Range(0, 3);
+        }
+
+        if (floatingTextPrefab)
+        {
+            ShowUpgrade();
+        }
+
+        if (pickUpNumber == 0)
+        {
+            Speed();
+        }
+        else if (pickUpNumber == 1)
+        {
+            Damage();
+        }
+        else if (pickUpNumber == 2)
+        {
+            HealthUp();
+        }
+    }
+
+    void ShowUpgrade()
+    {
+        var go = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
+        if (pickUpNumber == 0)
+        {
+            go.GetComponent<TextMesh>().text = "Speed Up";
+
+        }
+        else if (pickUpNumber == 1)
+        {
+            go.GetComponent<TextMesh>().text = "Damage Up";
+
+        }
+        else if (pickUpNumber == 2)
+        {
+            go.GetComponent<TextMesh>().text = "Health Up";
+
+        }
+    }
+    void Speed() //Increases Speed
+    {
+        move.moveSpeed += speedUp;
+        Debug.Log("SpeedUp active");
+    }
+
+    void Damage() //Increases Damage
+    {
+        hurt.bulletDamage += damageUp;
+        Debug.Log("DamageUp active");
+    }
+
+    void HealthUp() //Increases health
+    {
+        objectHealth += healthUp;
+        Debug.Log("HealthUp active");
+    }
+
 }
