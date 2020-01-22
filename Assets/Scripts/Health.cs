fbd4fb69c13412c80e0cf40f5ab921eff11f51df
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2020-01-22 08:35:14 +0100

    no message

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,109 +5,110 @@ using UnityEngine.Audio;
 
 public class Health : MonoBehaviour
 {
-    public int objectHealth;
-    public AudioClip hitSound1;
-    public AudioClip hitSound2;
+	public int health;
+	public int maxHealth;
+	public AudioClip hitSound1;
+	public AudioClip hitSound2;
 
-    public int speedUp;
-    public int damageUp;
-    public int healthUp;
+	public int speedUp;
+	public int damageUp;
+	public int healthUp;
 
-    public int pickUpNumber;
-    public GameObject floatingTextPrefab;
+	public int pickUpNumber;
+	public GameObject floatingTextPrefab;
 
-    //Refrencing the scripts
-    public MovementTest move;
-    public PlayerShooting hurt;
-
-
-    private void Start()
-    {
-        //SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-        move = GameObject.FindObjectOfType<MovementTest>();
-        hurt = GameObject.FindObjectOfType<PlayerShooting>();
-    }
-
-    void Update()
-    {
-        if (objectHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-        }
-    }
-
-    public void DoDamage(int damage)
-    {
-        objectHealth -= damage;
-        SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
-    }
+	//Refrencing the scripts
+	public MovementTest move;
+	public PlayerShooting hurt;
 
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.tag == "Pickup")
-        {
-            pickUpNumber = Random.Range(0, 3);
-        }
+	private void Start()
+	{
+		//SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+		move = GameObject.FindObjectOfType<MovementTest>();
+		hurt = GameObject.FindObjectOfType<PlayerShooting>();
+	}
 
-        if (floatingTextPrefab)
-        {
-            ShowUpgrade();
-        }
+	void Update()
+	{
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+		}
+	}
 
-        if (pickUpNumber == 0)
-        {
-            Speed();
-        }
-        else if (pickUpNumber == 1)
-        {
-            Damage();
-        }
-        else if (pickUpNumber == 2)
-        {
-            HealthUp();
-        }
-    }
+	public void DoDamage(int damage)
+	{
+		health -= damage;
+		SoundManager.instance.RandomizeSfx(hitSound1, hitSound2);
+	}
 
-    void ShowUpgrade()
-    {
-        var go = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
-        if (pickUpNumber == 0)
-        {
-            go.GetComponent<TextMesh>().text = "Speed Up";
 
-        }
-        else if (pickUpNumber == 1)
-        {
-            go.GetComponent<TextMesh>().text = "Damage Up";
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Pickup")
+		{
+			pickUpNumber = Random.Range(0, 3);
+		}
 
-        }
-        else if (pickUpNumber == 2)
-        {
-            go.GetComponent<TextMesh>().text = "Health Up";
+		if (floatingTextPrefab)
+		{
+			ShowUpgrade();
+		}
 
-        }
-    }
-    void Speed() //Increases Speed
-    {
-        move.moveSpeed += speedUp;
-        Debug.Log("SpeedUp active");
-    }
+		if (pickUpNumber == 0)
+		{
+			Speed();
+		}
+		else if (pickUpNumber == 1)
+		{
+			Damage();
+		}
+		else if (pickUpNumber == 2)
+		{
+			HealthUp();
+		}
+	}
 
-    void Damage() //Increases Damage
-    {
-        hurt.bulletDamage += damageUp;
-        Debug.Log("DamageUp active");
-    }
+	void ShowUpgrade()
+	{
+		var go = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity, transform);
+		if (pickUpNumber == 0)
+		{
+			go.GetComponent<TextMesh>().text = "Speed Up";
 
-    void HealthUp() //Increases health
-    {
-        objectHealth += healthUp;
-        Debug.Log("HealthUp active");
-    }
+		}
+		else if (pickUpNumber == 1)
+		{
+			go.GetComponent<TextMesh>().text = "Damage Up";
+
+		}
+		else if (pickUpNumber == 2)
+		{
+			go.GetComponent<TextMesh>().text = "Health Up";
+
+		}
+	}
+	void Speed() //Increases Speed
+	{
+		move.moveSpeed += speedUp;
+		Debug.Log("SpeedUp active");
+	}
+
+	void Damage() //Increases Damage
+	{
+		hurt.bulletDamage += damageUp;
+		Debug.Log("DamageUp active");
+	}
+
+	void HealthUp() //Increases health
+	{
+		health += healthUp;
+		Debug.Log("HealthUp active");
+	}
 
 }
