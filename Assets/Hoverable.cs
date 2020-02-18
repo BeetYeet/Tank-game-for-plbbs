1c22d9051a6ef8e146adc84d782e7e293066b697
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2020-02-18 17:21:13 +0100

    menubuttons animated

---
+++ Assets/Hoverable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Hoverable : MonoBehaviour
+{
+	Button button;
+	Animator anim;
+	void Start()
+	{
+		button = GetComponent<Button>();
+		anim = GetComponent<Animator>();
+	}
+
+	public void Hover()
+	{
+		anim.SetBool("IsHovered", true);
+	}
+	public void DeHover()
+	{
+		anim.SetBool("IsHovered", false);
+	}
+}
---
+++ Assets/KeyboardButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class KeyboardButton : MonoBehaviour
+{
+	Button button;
+	static bool hasTakenInputThisFrame = false;
+	void Start()
+	{
+		button = GetComponent<Button>();
+	}
+
+	private void Update()
+	{
+		hasTakenInputThisFrame = false;
+	}
+	void LateUpdate()
+	{
+		if (hasTakenInputThisFrame || EventSystem.current.currentSelectedGameObject != gameObject)
+			return;
+		if (Input.GetButtonDown("Gas_P1") || Input.GetButtonDown("Gas_P2"))
+		{
+			if (button.navigation.selectOnUp != null)
+				EventSystem.current.SetSelectedGameObject(button.navigation.selectOnUp.gameObject);
+			Debug.Log("Button Up");
+		}
+
+		if (Input.GetButtonDown("Brake_P1") || Input.GetButtonDown("Brake_P2"))
+		{
+			if (button.navigation.selectOnDown != null)
+				EventSystem.current.SetSelectedGameObject(button.navigation.selectOnDown.gameObject);
+			Debug.Log("Button Down");
+		}
+		hasTakenInputThisFrame = true;
+	}
+
+}
